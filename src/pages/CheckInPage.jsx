961282ec77fc93f2c2bcb6e8db9ca451bd0c4aeb
saumyasriv <saumya.sriv9@gmail.com>
2026-09-06 21:59:05 +0530

    Update CheckInPage.jsx

--- src/pages/CheckInPage.jsx
+++ src/pages/CheckInPage.jsx
@@ -21,12 +21,9 @@ function CheckInPage() {
   const classTimings = [
     "6:00 AM",
     "7:00 AM",
-    "8:00 AM",
-    "9:00 AM",
-    "5:00 PM",
+    "8:15 AM",
     "6:00 PM",
-    "7:00 PM",
-    "8:00 PM"
+    "10:00 PM"
   ];
 
   const isAdmin =
